491cdefa997067704f692fa1e35895883ca42ad7
hirokirokki0820 <hirokirokki0820@gmail.com>
2023-02-09 10:26:59 +0900

    adminモデルの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   root "home#top"
+
+  devise_for :admins, controllers: {
+    registrations: "admins/registrations",
+    sessions: "admins/sessions",
+    passwords: "admins/passwords",
+    confirmations: "admins/confirmations"
+  }
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions",
-    passwords: "users/passwords"
+    passwords: "users/passwords",
+    confirmations: "users/confirmations"
   }
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
